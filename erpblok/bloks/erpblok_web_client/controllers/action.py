513076b6a2e5382bb5d30aa42cb2704a5c3bbff5
Georges Racinet on ishtar.racinet.fr <georges@racinet.fr>
2015-05-24 23:43:30 +0200

    File should not have ben committed

--- erpblok/bloks/erpblok_web_client/controllers/action.py
+++ erpblok/bloks/erpblok_web_client/controllers/action.py
@@ -4,9 +4,6 @@ from anyblok import Declarations
 register = Declarations.register
 PyramidJsonRPC = Declarations.PyramidJsonRPC
 
-Sec = Declarations.SecurityManager
-
-READ = Declarations.Permission.read
 
 @register(PyramidJsonRPC)
 class Action:
@@ -18,9 +15,6 @@ class Action:
         :param action: id of the action """
         UIAction = self.registry.UI.Action
         action = UIAction.query().filter(UIAction.id == int(action)).first()
-        principals = pyramid_get_principals()
-        if not Sec.check_permission(self, principals, READ):
-            return 403 # ce qu'il faut
         return action.render()
 
 
